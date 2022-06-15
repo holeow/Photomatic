fe78bd06116d4111a7a1062e5db5ae3bd45c9d31
holeow <48766903+holeow@users.noreply.github.com>
2022-06-15 16:04:57 +0200

    cleaned up code (#4)
    *added some comments
    *moved some functions into viewmodel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//? Test folders
+//?? Test folders
 let mainFolder = new Folder("root", "cat", null);
 mainFolder.generatePhotos(12, 480, 270);
 
@@ -21,13 +21,21 @@ mainFolder.folders.push(boat);
 mainFolder.folders.push(eiffel);
 mainFolder.folders.push(dogs);
 
-
-mainFolder.openFolder();
+NormalViewModel.openFolder(mainFolder);
+//mainFolder.openFolder();
 //? end of test folders
 
-//? Gestion des boutons
+
+
+
+
+
+
+
+//?? Gestion des boutons
 document.querySelector("#HomeButton").addEventListener("click", () => {
-    mainFolder.openFolder();
+    NormalViewModel.openFolder(mainFolder);
+    
 });
 
 
@@ -74,29 +82,36 @@ putbutton.addEventListener("click",putImages );
 
 //? Fin de la gestion des boutons
 
+
+
+
+
+//?? Fonctions associées aux boutons et aux windows
+
 function putImages(){
     for(const container of document.querySelectorAll("#images .imgcontainer")){
         container.classList.toggle("selectedformove",false);
     }
 
-    for(const img of Folder.selectedMovePhotos){
-        if(img[0] == Folder.currentFolder){
+    for(const img of NormalViewModel.selectedMovePhotos){
+        if(img[0] == NormalViewModel.currentFolder){
             
             continue;
         }
         else{
           let index=  img[0].photos.findIndex(a=> img[1]== a);
           img[0].photos.splice(index,1);
-          Folder.currentFolder.photos.push(img[1]);
+          NormalViewModel.currentFolder.photos.push(img[1]);
         }
     }
-    Folder.selectedMovePhotos = [];
+    NormalViewModel.selectedMovePhotos = [];
 }
 
 function okayUploadImage() {
     let photo = new Photo(0, 0, 0, null, imageUploader.querySelector("input").value);
-    Folder.currentFolder.photos.push(photo);
-    Folder.currentFolder.displayImages();
+    NormalViewModel.currentFolder.photos.push(photo);
+    NormalViewModel.displayImages(NormalViewModel.currentFolder);
+    
     closeAllWindows();
 }
 
@@ -126,11 +141,12 @@ function okayCreateFolder() {
 
     if (input.length > 0) {
 
-        let currentFolder = Folder.currentFolder;
+        let currentFolder = NormalViewModel.currentFolder;
         let folder = new Folder(input, null, currentFolder);
 
         currentFolder.folders.push(folder);
-        currentFolder.openFolder();
+        NormalViewModel.openFolder(currentFolder);
+        
         createFolderClick();
         closeAllWindows();
     }
@@ -153,49 +169,4 @@ function createFolderClick() {
 function menuClick() {
     leftNavBar.classList.toggle("mobileHide");
 }
-
-/**
- * Ajoute un dossier au menu lateral
- * @param {string} name le nom du dossier
- * @param {Folder} folder l'objet du dossuer
- */
-function addDossier(name, folder) {
-
-    let folderPanel = document.createElement("div");
-    folderPanel.classList.add("folderPanel");
-
-    folderPanel.folder = folder;
-
-    let icon = document.createElement("i");
-    icon.classList.add("featherfolder");
-    icon.setAttribute("data-feather", "folder");
-    icon.setAttribute("stroke", "black");
-    icon.setAttribute("fill", "#f8d775");
-
-    folderPanel.appendChild(icon);
-
-    let textDiv = document.createElement("div");
-    textDiv.classList.add("textDiv");
-    folderPanel.appendChild(textDiv);
-
-    textDiv.addEventListener("click", () => {
-        folderPanel.folder.openFolder();
-
-    });
-
-    let p = document.createElement("p");
-    p.textContent = name;
-
-    textDiv.appendChild(p);
-
-    let minusIcon = document.createElement("i");
-    minusIcon.setAttribute("data-feather", "folder-minus");
-    minusIcon.classList.add("featherfolderright");
-
-    folderPanel.appendChild(minusIcon);
-
-    document.querySelector("main #leftNavBar").appendChild(folderPanel);
-
-    feather.replace();
-
-}
+//? Fin des fonctions associées aux boutons et aux windows
--- assets/js/viewmodel.js
+++
@@ -1,7 +0,0 @@
-class NormalViewModel{
-/**@type {Folder} */
-currentFolder = null;
-
-/**@type {Array.<Array.<(Folder|Photo)>>} */
-selectedMovePhotos = [];
-}
